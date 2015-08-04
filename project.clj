e8d273dbf0ba670b9a57aeade8e6119635619add
Paul Lam <paul@quantisan.com>
2015-08-04 15:25:05 +0300

    update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject functional-music "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "a gentle introduction to functional programming through making music"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
