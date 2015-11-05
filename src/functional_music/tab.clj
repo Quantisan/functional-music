8eaff973e9c4bea318b1dbd050ffabc886f373e8
Paul Lam <paul@quantisan.com>
2015-11-05 16:00:05 +0100

    Revert "add hints"
    This reverts commit 1a1cd2f67b22129283467ad04d21883fbc5505e6.
    
    Revert "removed tab fn to do as exercise"
    
    This reverts commit 723a0ed7bb9fa34d2b2a9bbc424cdbf4cc7b8efc.

--- src/functional_music/tab.clj
+++ src/functional_music/tab.clj
@@ -13,9 +13,11 @@
 
 (defn filter-tab-lines
   "Only keep the lines that are tabs. Assuming that tab lines must have a '-' and '|' characters."
-  ; hint: filter, defn, .contains, ->, ->>
   [coll]
-  )
+  (->> coll
+       ;; filter lines with '-' and '|' characters
+       (filter #(.contains % "-"))
+       (filter #(.contains % "|"))))
 
 (defn- join-tabs
   "Join two sets of tabs together"
@@ -30,15 +32,19 @@
        (reduce join-tabs)))
 
 (defn remove-format [coll]
-  ; hint: map, s/replace, #
-  )
+  (->> coll
+       (map #(s/replace % #"\|" ""))
+       (map #(s/replace % #"^[A-z]" ""))))
 
 (defn to-string-fret [[s f]]
   [s (java.lang.Character/getNumericValue f)])
 
 (defn tab->string-fret [e B G D A E]
-  ; hint: map-indexed, vector, filter, re-seq, map
-  )
+  (->> [E A D G B e]
+       (map-indexed vector)
+       ;; coerce char to string then check with regex
+       (filter #(re-seq #"[0-9]" (str (second %))))
+       (map to-string-fret)))
 
 (defn parse-guitar-tab [s]
   (->> s
